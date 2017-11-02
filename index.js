265ac91f1856fb70dae04096e7d72e390bb43bad
zacharytaylor <zachtylr04@gmail.com>
2017-11-01 21:30:57 -0600

    stuff

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ io.on('connection', function(socket){
         io.emit('chat message', msg);
       });        
 });
-      
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
--- javascript/message.js
+++ javascript/message.js
@@ -1,18 +1,18 @@
-$(function () {
+var username = "";
+
+(function () {
     var socket = io();
     $('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
     });
     socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
+        $('#messages').append($('<li>').text(msg));
     });
 });
 
 $(document).ready(function (){
-        var name=prompt("Please enter your username");
-        if (name!=null){
-          alert("Username was " + name);
-       }
-    });
+    username = prompt("Please enter your username");
+    $("#display-username").text("Username: " + username);
+});
