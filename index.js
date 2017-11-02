62f02247edc5185619d95b60c85737476ed457d1
zacharytaylor <zachtylr04@gmail.com>
2017-11-01 22:18:31 -0600

    connections and typing

--- index.js
+++ index.js
@@ -8,18 +8,34 @@ app.use(express.static(path.join(__dirname, 'javascript')));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
-    });
+});
+
+var clients = [];
+var client_names = [];
 
 io.on('connection', function(socket){
     console.log('a user connected');
+    socket.on('new user', function(usr) {
+      clients.push(socket);
+      client_names.push(usr);
+      io.emit('new user', usr);
+    });
     socket.on('disconnect', function(){
-        console.log('user disconnected');
-      });
-      socket.on('chat message', function(usr, msg){
-        console.log('user: ' + usr);
-        console.log('message: ' + msg);
-        io.emit('chat message', usr, msg);
-      });        
+      var k = clients.indexOf(socket);
+      name = client_names[k];
+      console.log('user ' + name + ' disconnected');
+      clients.splice(k, 1);
+      client_names.splice(k, 1);
+      io.emit('user disconnect', name);
+    });
+    socket.on('chat message', function(usr, msg){
+      console.log('user: ' + usr);
+      console.log('message: ' + msg);
+      io.emit('chat message', usr, msg);
+    });
+    socket.on('user typing', function(usr) {
+      io.emit('user typing', usr);
+    });
 });
 
 http.listen(3000, function(){
--- javascript/message.js
+++ javascript/message.js
@@ -1,18 +1,36 @@
 var username = "";
+var socket = io();
 
 $(function () {
-    var socket = io();
     $('form').submit(function(){
         socket.emit('chat message', username, $('#m').val());
         $('#m').val('');
         return false;
     });
+
     socket.on('chat message', function(usr, msg){
         $('#messages').append($('<li>').text(usr + ": " + msg));
+    });
+
+    socket.on('new user', function(usr) {
+        $('#messages').append($('<li>').text(usr + " joined!"));
+    });
+
+    socket.on('user disconnect', function(usr) {
+        $('#messages').append($('<li>').text(usr + " left."));
+    });
+
+    socket.on('user typing', function(usr) {
+        $('display-typing').text(usr + " is typing...");
     });
 });
 
 $(document).ready(function (){
     username = prompt("Please enter your username");
     $("#display-username").text("Username: " + username);
+    socket.emit('new user', username);
+
+    $("m").keyup(function() {
+        socket.emit('user typing', username);
+    });
 });
